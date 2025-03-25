1e7279d143046ed87971692843f8be092a601e7c
JMasalski <masalski.kuba123@gmail.com>
2025-03-25 17:38:41 +0100

    enhance InvoiceForm; add product management with dynamic fields for name, quantity, price, tax rate, and unit selection

--- client/src/constants/sidebarLinks.js
+++ client/src/constants/sidebarLinks.js
@@ -1,4 +1,4 @@
-import {Home, FileText, PlusSquare, Users,  Settings} from "lucide-react";
+import {Home, FileText, PlusSquare, Users, Settings} from "lucide-react";
 
 
 export const sidebarLinks = [
@@ -6,6 +6,7 @@ export const sidebarLinks = [
     { name: "Invoices", path: "/invoices", icon: FileText },
     { name: "New Invoice", path: "/invoices/new", icon: PlusSquare },
     { name: "Clients", path: "/clients", icon: Users },
+
     { name: "Settings", path: "/settings", icon: Settings, isBottom:true },
 
 ]

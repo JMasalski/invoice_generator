d4ea8b1de6cabaeaeeaf00388bb4994547348ac2
JMasalski <masalski.kuba123@gmail.com>
2025-03-31 22:29:56 +0200

    fixed url

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: 'https://invoice-generator-frontend-a8a9.onrender.com/',
+        origin: 'https://invoice-generator-frontend-a8a9.onrender.com',
         credentials: true
     }
 ))

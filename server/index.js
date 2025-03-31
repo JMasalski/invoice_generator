2b87771f3d325bf32d01c9a896df523ee1bd25af
JMasalski <masalski.kuba123@gmail.com>
2025-03-31 21:32:35 +0200

    fix

--- server/index.js
+++ server/index.js
@@ -13,37 +13,37 @@ import invoiceRoutes from "./routes/invoice.routes.js";
 
 const app = express();
 
-const allowedOrigins = [
-    'http://localhost:5173',
-    'https://invoice-generator-sp2e.vercel.app'
-];
-
-app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, origin);  // 👈 Zwracaj konkretny origin, a nie "*"
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true
-}));
-
-// Obsługa preflight requestów (OPTIONS)
-app.options('*', (req, res) => {
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.sendStatus(200);
-});
+// const allowedOrigins = [
+//     'http://localhost:5173',
+//     'https://invoice-generator-sp2e.vercel.app'
+// ];
+//
+// app.use(cors({
+//     origin: (origin, callback) => {
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, origin);  // 👈 Zwracaj konkretny origin, a nie "*"
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     credentials: true
+// }));
+//
 // app.use(cors());
-// app.use(cors(
-//     {
-//         origin: ['http://localhost:5173', 'https://invoice-generator-sp2e.vercel.app'],
-//         credentials: true
-//     }
-// ))
+// // Obsługa preflight requestów (OPTIONS)
+// app.options('*', (req, res) => {
+//     res.header("Access-Control-Allow-Origin", req.headers.origin);
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     res.sendStatus(200);
+// });
+app.use(cors(
+    {
+        origin: ['http://localhost:5173', 'https://invoice-generator-sp2e.vercel.app'],
+        credentials: true
+    }
+))
 app.use(cookieParser())
 app.use(express.json());
 dotenv.config();

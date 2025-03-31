37ef14d390f03c359f6d33663e06ff9d8c8de6b7
JMasalski <masalski.kuba123@gmail.com>
2025-03-31 21:00:36 +0200

    enhance CORS configuration; allow requests from production URL

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: ['http://localhost:5173', 'https://invoice-generator-sp2e.vercel.app'],
         credentials: true
     }
 ))
